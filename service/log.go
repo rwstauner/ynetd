066e2f32b301901c09b755e1e9245e1c6c30f9f4
Randy Stauner <randy@magnificent-tears.com>
2017-12-21 07:54:36 -0700

    Fix SetLogger comments

--- service/log.go
+++ service/log.go
@@ -7,7 +7,7 @@ import (
 
 var logger = log.New(os.Stdout, "ynetd ", log.Ldate|log.Ltime|log.Lmicroseconds)
 
-// SetLogger sets the logger used in the procman package.
+// SetLogger sets the logger used in this package.
 func SetLogger(l *log.Logger) {
 	logger = l
 }
